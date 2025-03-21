51720f766cc719a0491bbe40ee8fa31eb53408fc
Loris Spalvieri <spalvieriloris19@gmail.com>
2025-03-21 12:44:18 +0100

    Primo Commit

---
+++ src/main/java/com/b2b/spring/boot/demo/Application.java
@@ -0,0 +1,37 @@
+package com.b2b.spring.boot.demo;
+
+import com.b2b.spring.boot.demo.client.EsercizioClient;
+import com.b2b.spring.boot.demo.controller.api.AuthenticationAPI;
+import com.b2b.spring.boot.demo.entity.ToDoRecord;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import java.util.Set;
+
+@SpringBootApplication
+@EnableFeignClients
+public class Application {
+
+  public static void main(String[] args) {
+
+    SpringApplication.run(Application.class, args);
+
+  }
+
+
+
+  @Autowired EsercizioClient esercizioClient;
+
+  @Autowired AuthenticationAPI api;
+
+//@PostConstruct
+//public void test() {
+//  Set<ToDoRecord> test = esercizioClient.test();
+//  System.out.println("DATI RECUPERATI: ");
+//  test.forEach(System.out::println);
+//}
+
+}
